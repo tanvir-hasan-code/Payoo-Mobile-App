eb1675aa43c976ea2f5ee97552ad4ce690ff8344
tanvir-hadan-code <tanvirx015@gmail.com>
2025-03-05 23:40:23 +0600

    toggle issu fix

--- script/login.js
+++ script/login.js
@@ -2,13 +2,14 @@
 document
   .getElementById("btn-login")
   .addEventListener("click", function (event) {
-    event.preventDefault();
+	  event.preventDefault();
+	  
     const accountNumber = document.getElementById("account-number").value;
 
 	  const accountPin = document.getElementById("account-pin").value;
 	  if (accountNumber.length === 11) {
 		  if (accountPin === '1234') {
-			  window.location.href ='../main.html'
+			  redirectToMain();
 		  } else {
 			  alert('This pin Don"t match')
 		  }
@@ -16,6 +17,9 @@ document
 		  alert('This account Number not valid.');
 	  }
   });
+  function redirectToMain() {
+	window.location.href = "../main.html"; 
+}
 
 
 
